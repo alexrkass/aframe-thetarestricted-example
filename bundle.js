a340aaceac28eee7c5cb3986c0d0e6f7e8cd8ce3
Alex Kass <alexrkass@gmail.com>
2016-01-14 06:30:02 -0800

    working

---
+++ bundle.js
@@ -0,0 +1,4 @@
+var AFRAME = require('aframe');
+var AFRAMECORE = require('aframe-core')
+var noClickLookControls = require('aframe-no-click-look-controls').component;
+AFRAMECORE.registerComponent('no-click-look-controls', noClickLookControls);
